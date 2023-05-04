488f91b788db095e955b79004caccf28b1208e2e
accpt27chen/zhangq <13631262097@139.com>
2023-05-04 10:30:50 +0800

    【新增】 1.前端路由增加前缀

--- ruoyi-ui/src/router/index.js
+++ ruoyi-ui/src/router/index.js
@@ -172,6 +172,7 @@ Router.prototype.push = function push(location) {
 
 export default new Router({
   mode: 'history', // 去掉url中的#
+  base: process.env.BASE_ROUTE,
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -11,6 +11,9 @@ const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
 
 const port = process.env.port || process.env.npm_config_port || 80 // 端口
 
+// 基础路由（二级路由）
+const BASE_ROUTE = '/ruoyi-vue'
+
 // vue.config.js 配置说明
 //官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
 // 这里只列一部分，具体配置参考文档
@@ -18,7 +21,7 @@ module.exports = {
   // 部署生产环境和开发环境下的URL。
   // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
   // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
-  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  publicPath: BASE_ROUTE, // process.env.NODE_ENV === "production" ? "/" : "/",
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
@@ -132,5 +135,15 @@ module.exports = {
           }
         }
       )
+
+    config.plugin('define').tap((args) => {
+      args[0]['process.env'].SERVER_URL = JSON.stringify(SERVER_URL)
+      args[0]['process.env'].BASE_ROUTE = JSON.stringify(BASE_ROUTE)
+      // args[0]['process.env'].CONFIG_BASE = JSON.stringify(CONFIG_BASE)
+      // args[0]['process.env'].CONFIG_URL = JSON.stringify(CONFIG_URL)
+      return args
+    })
+    // 就这一行，让你爽到飞
+    config.plugin('CompressionPlugin').use(CompressionPlugin, [])
   }
 }
